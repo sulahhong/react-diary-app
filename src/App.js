4a58bee9df92d97f5707866ee5db75b3f91d6186
sulahhong <100662448+sulahhong@users.noreply.github.com>
2022-04-15 11:58:25 +0900

    page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from './pages/Home';
+import New from './pages/New';
+import Edit from './pages/Edit';
+import Diary from './pages/Diary';
+import RouteTest from './components/RouteTest';
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <Switch>
+        <Route path='/'>
+          <Home />
+        </Route>
+        <Route path='/new'>
+          <New />
+        </Route>
+        <Route path='/edit'>
+          <Edit />
+        </Route>
+        <Route path='/diary'>
+          <Diary />
+        </Route>
+        <RouteTest />
+      </Switch>
+    </Router> 
   );
 }
 
