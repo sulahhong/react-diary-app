fdefa83e737f50c28e53134bced9398285b9b26a
sulahhong <100662448+sulahhong@users.noreply.github.com>
2022-05-03 19:39:43 +0900

    뼈대

--- src/App.js
+++ src/App.js
@@ -1,33 +1,33 @@
 import './App.css';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import RouteTest from './components/RouteTest';
+
 import Home from './pages/Home';
 import New from './pages/New';
 import Edit from './pages/Edit';
 import Diary from './pages/Diary';
-import RouteTest from './components/RouteTest';
 
 
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path='/'>
-          <Home />
-        </Route>
-        <Route path='/new'>
-          <New />
-        </Route>
-        <Route path='/edit'>
-          <Edit />
-        </Route>
-        <Route path='/diary'>
-          <Diary />
-        </Route>
-        <RouteTest />
-      </Switch>
-    </Router> 
+    <BrowserRouter>
+      <div className='APP'>
+        <h2>haha</h2>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/new' element={<New />} />
+        <Route path='/edit' element={<Edit />} />
+        <Route path='/diary/:id' element={<Diary />} />
+        {/* <Route path='/diary' element={<Diary />} /> */}
+      </Routes>
+      <RouteTest />
+      </div>
+    </BrowserRouter>
   );
 }
 
 export default App;
+
+
