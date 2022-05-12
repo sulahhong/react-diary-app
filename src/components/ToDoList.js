c5ca203c6607bfcb4da947e46c6865276a92b069
sulahhong <100662448+sulahhong@users.noreply.github.com>
2022-05-13 00:52:05 +0900

    edit fuction

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,14 +1,15 @@
 import { useState, useRef} from "react";
 
 
-const ToDoList = ({todoList}) => {
+const ToDoList = () => {
   const [toDos, setToDos] = useState([]);
+  const [currentContent, setCurrentContent]= useState("")
 
   const dataId2 = useRef(0);
 
   const onCreateToDo = (content) => {
     const newItem = {
-      content,
+      currentContent,
       id: dataId2.current,
     };
     dataId2.current += 1;
@@ -19,13 +20,18 @@ const ToDoList = ({todoList}) => {
     <div className="ToDoList">
       <h2>TODOLIST</h2>
 
-      <input className="ToDoList" autoFocus placeholder="오늘의 할일은?"/> 
-      <button>작성</button>
+      <input 
+        className="ToDoList" autoFocus placeholder="오늘의 할일은?"
+        value={currentContent}
+        onChange={(e) => setCurrentContent(e.target.value)}/> 
+      
+      <button onClick={onCreateToDo}>작성</button>
       <div>
-            <h4>{todoList.length}개의 할일이 있어요</h4>
-                {todoList.map((it) => (
-                    <div key={it.id}>{it.content}</div>
+            <h4>{toDos.length}개의 할일이 있어요</h4>
+                {toDos.map((it) => (
+                    <div key={it.id}>{it.currentContent}</div>
                 ))}
+
       </div>
     </div>
   );
