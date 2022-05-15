5e44b38fa01e6539523ba4e744de3b6787964cd0
sulahhong <100662448+sulahhong@users.noreply.github.com>
2022-05-15 21:28:38 +0900

    localstorage

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,25 +1,45 @@
-import { useState, useRef} from "react";
+import { useState, useRef, useReducer, useEffect} from "react";
 import { GrCheckbox, GrCheckboxSelected } from "react-icons/gr"
 import {ImBin} from "react-icons/im"
 
+const getLocalStorage=()=>{
+  let storage=localStorage.getItem('todoList')
+  if(storage){
+    return JSON.parse(storage)
+  }else{
+    return [];
+  }
+}
+
 const ToDoList = () => {
-  const [toDos, setToDos] = useState([]);
+  const [toDos, setToDos] = useState(getLocalStorage());
   const [currentContent, setCurrentContent]= useState("")
+
+
 
   const dataId2 = useRef(0);
 
+
   const onCreateToDo = (content) => {
-    const newItem = {
+    let newItem = {
       currentContent,
       id: dataId2.current,
+
     };
     dataId2.current += 1;
     setToDos([newItem, ...toDos])
+    
   }
+  useEffect(()=>{
+    window.localStorage.setItem ("todoList", JSON.stringify(toDos))
+  }, [toDos])
 
   const onDeleteToDo = (targetId) => {
     const newToDoList = toDos.filter((it) => it.id !== targetId);
+
     setToDos(newToDoList)
+    
+    console.log(toDos)
   }
 
 
@@ -32,11 +52,12 @@ const ToDoList = () => {
         value={currentContent}
         onChange={(e) => setCurrentContent(e.target.value)}/> 
       
-      <button onClick={onCreateToDo}>작성</button>
+      <button onClick={onCreateToDo} >작성</button>
       <div>
             <h4>{toDos.length}개의 할일이 있어요</h4>
                 {toDos.map((it) => (
-                    <div key={it.id}><GrCheckbox />{it.currentContent} <ImBin onClick={onDeleteToDo}/></div>
+                    <div key={it.id}>
+                      <GrCheckbox />{it.currentContent}<ImBin  /></div>
                     //the checkbox is selected change the icon  <GrCheckboxSelected />
                 ))}
 
